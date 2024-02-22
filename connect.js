e0888d408f9d1061d566109701006170aebf784d
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-02-22 16:34:22 +0200

    Update connect.js

--- connect.js
+++ connect.js
@@ -2,11 +2,16 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
+  connectionLimit: 10,
+  acquireTimeout: 10000,
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  insecureAuth: true,
 });
+
 
 //променливите се намират в .env за повече сигурност
