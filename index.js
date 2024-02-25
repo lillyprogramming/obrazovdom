24d61d74ec998b8b24efb7bb4a0d29c2d36c0ead
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-02-25 12:22:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 8800;
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/tmp");
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
