f209aef4a75d9e8822e262fa21dc2c63a2f9c0ed
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-02-25 13:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ import cors from "cors";
 import multer from "multer";
 //multer е за качване и запазване на изображението в базата данни. Нужно е да се конфигурира и в бакенда
 import cookieParser from "cookie-parser";
-import { fileURLToPath, pathToFileURL } from "url";
-import { dirname } from "path";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -19,34 +17,19 @@ const org = "https://obrazovdom.com";
 // const org = "http://localhost:3000";
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use(express.json());
-
-const corsOptions = {
-  origin: org,
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ credentials: true, origin: org }));
 //credentials: true - много важно!
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 8800;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-import { join } from "path";
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    // Use path.join to resolve the correct path
-    cb(null, join(__dirname, "../client/public/upload"));
+    cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
