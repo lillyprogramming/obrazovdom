566928e54b5e97cb65f0f708385fa93248dfab5a
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-02-27 01:40:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../client/public/upload");
+    cb(null, "/tmp");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + file.originalname);
