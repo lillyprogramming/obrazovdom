b28454c1dd22f9dc8fb23b61249f10fcb1190522
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-02-27 21:26:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import commentRoutes from "./routes/comments.js";
 import todoRoutes from "./routes/todos.js";
 import groceryRoutes from "./routes/grocery.js";
 import cors from "cors";
-import multer from "multer";
+// import multer from "multer";
 //multer е за качване и запазване на изображението в базата данни. Нужно е да се конфигурира и в бакенда
 import cookieParser from "cookie-parser";
 
@@ -24,30 +24,30 @@ app.use(cors({ credentials: true, origin: org }));
 //credentials: true - много важно!
 app.use(cookieParser());
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
-app.post('/posts', upload.single('image'), async (req, res) => {
-    try {
-        const { descr, img } = req.body;
+// app.post('/posts', upload.single('image'), async (req, res) => {
+//     try {
+//         const { descr, img } = req.body;
         
-        // Assuming img is a base64-encoded string
-        const base64Image = img.split(';base64,').pop();
+//         // Assuming img is a base64-encoded string
+//         const base64Image = img.split(';base64,').pop();
 
-        // Insert the data into your MySQL database
-        const sql = 'INSERT INTO images (description, base64data) VALUES (?, ?)';
-        connection.query(sql, [descr, base64Image], (err, result) => {
-            if (err) {
-                console.error(err);
-                return res.status(500).send('Internal Server Error');
-            }
-            res.status(200).send('Image uploaded successfully');
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-    }
-});
+//         // Insert the data into your MySQL database
+//         const sql = 'INSERT INTO images (description, base64data) VALUES (?, ?)';
+//         connection.query(sql, [descr, base64Image], (err, result) => {
+//             if (err) {
+//                 console.error(err);
+//                 return res.status(500).send('Internal Server Error');
+//             }
+//             res.status(200).send('Image uploaded successfully');
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/family", familyRoutes);
