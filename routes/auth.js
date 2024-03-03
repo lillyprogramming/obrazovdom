ce5dd778c9dc0c2d2a3943be647907ed9f9dd6b3
Lilly D <78252122+lillyprogramming@users.noreply.github.com>
2024-03-03 16:16:42 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { logIn, register } from "../controllers/auth.js";
+import { logIn, register, logOut} from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/login", logIn);
 router.post("/register", register);
-// router.post("/logout", logOut);
+router.post("/logout", logOut);
 
 export default router;
